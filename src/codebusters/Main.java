9e794e129d63b84c0c012f218e3de6f811e2cfda
samiabout <sami-about@hotmail.fr>
2016-06-28 10:33:11 +0200

    ajout de la fonction stun() et amélioration de la fonction travel()

--- src/codebusters/Main.java
+++ src/codebusters/Main.java
@@ -24,7 +24,15 @@ class Player {
 	 * 
 	 * ajouter la fonciton stun
 	 * 
-	 * enregistre position présumées des ghost non capturés
+	 * peut-être qu'il faut compter un tour sur 2 pour stun. non nécessaire
+	 * 
+	 * prendre en compet state = 2 pour stun
+	 * ===idées d'amélioration===
+	 * enregistre position présumée des ghost non capturés
+	 * 
+	 * améliorer la fonction travel
+	 * 
+	 * prendre en compte les cas d'égalité
 	 */
 
 	static Ghost[] ghosts;
@@ -34,11 +42,14 @@ class Player {
 	static int ghostCount;
 	static int myTeamId;
 	
-	static int boardTravel=0;//travel the board if no ghost is visible 
+	static int boardTravelEven=0;//travel the board if no ghost is visible 
+	static int boardTravelOdd=0;
 	
     public static int ghostIndex;   //nb of each entity visble
     static int bustersIndex;
     static int opponentBustersIndex;
+    
+    static int nbturn;
 	
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
@@ -58,15 +69,23 @@ class Player {
 			ghosts[i]=new Ghost(i);
 		}
         
-
+        nbturn = 0;
         // game loop
         while (true) {
+        	nbturn++;
+        	
+            for (int i = 0; i < bustersPerPlayer; i++) {//set visible to false for all before correction
+    			opponentBusters[i].setVisible(false);
+    		}
+            for (int i = 0; i < ghostCount; i++) {
+    			ghosts[i].setVisible(false);
+    		}
+            
             int entities = in.nextInt(); // the number of busters and ghosts visible to you
             
             ghostIndex=0;   //nb of each entity visble
             bustersIndex=0;
             opponentBustersIndex=0;
-
             
             for (int i = 0; i < entities; i++) {
                 int entityId = in.nextInt(); // buster id or ghost id
@@ -84,8 +103,8 @@ class Player {
 				} else if (type == myTeamId+1) {//ghosts    (myTeamId+1 is not a constant => not suitable for switch/case)
 					ghosts[entityId].update(x, y,value);
 					ghostIndex++;
-				} else { //=-1 || =1   && !ghost => opponent ghost
-					opponentBusters[entityId].update(x, y, state,value);
+				} else { //=-1 || =1   && !ghost => opponent ghost //TODO il y a un bug ici à corriger
+					opponentBusters[entityId-bustersPerPlayer].update(x, y, state,value);
 					opponentBustersIndex++;
 				}
 				//System.err.println(bustersIndex);System.err.println(opponentBustersIndex);System.err.println(ghostIndex);
@@ -110,10 +129,12 @@ class Buster {//My busters
 	private int y;	
 	private int state;
 	private int value;
+	private int stunTurn;//last turn he has used stun()
 	private String answer;
 	
 	public Buster(int id) {
 		this.id=id;
+		this.stunTurn=-30;
 	}	
 	
 	public void update(int x, int y,int state,int value) {//change to update
@@ -127,12 +148,14 @@ class Buster {//My busters
 
 
 	public String action(){
-		if(!this.unload()){
-			if(!this.ghostAround()){
-				if(!this.closestGhost()){
-				    travel();
-				    
-				    }
+		if(!this.unload()){						//if he has a ghost
+			if(!this.stun()){					//if he can stun an opponent
+				if(!this.ghostAround()){		//if he can bust a ghost
+					if(!this.closestGhost()){	//if he can go to catch a ghost
+					    travel();				//if these is no ghost visible
+					    
+					    }
+				}	
 			}
 		}
 		return answer;
@@ -152,14 +175,39 @@ class Buster {//My busters
 				if((x-16000)*(x-16000)+(y-9000)*(y-9000)>1600*1600){
 					answer="MOVE 16000 9000";
 				}else answer="RELEASE";
-			}			
+			}
+			System.err.println("unload");
 			return true;
 		} 
 		return false;
 	}
 	
+	
+
+	private boolean stun() {//stun an opponent if close enough
+		if(this.canBust()){
+			for (int i = 0; i < Player.bustersPerPlayer; i++) {
+				if(Player.opponentBusters[i].getVisible() && Player.opponentBusters[i].canBeBusted()){
+					int OX=Player.opponentBusters[i].getX();
+					int OY=Player.opponentBusters[i].getY();
+					int BX=this.x;
+					int BY=this.y;
+					if((OX-BX)*(OX-BX)+(OY-BY)*(OY-BY)<1760*1760){
+						answer="STUN "+i;
+						stunTurn=Player.nbturn;
+						Player.opponentBusters[i].setStunedTurn(Player.nbturn);
+						System.err.println("stun");
+						return true;
+					}
+				}
+			}			
+		}
+
+		return false;
+	}
+
+	
 	private boolean ghostAround(){//if a ghost around, bust
-	System.err.println(Player.ghostIndex);
 		for (int i = 0; i < Player.ghostCount; i++) {
 			if(Player.ghosts[i].getVisible()){
 			
@@ -167,10 +215,9 @@ class Buster {//My busters
 				int GY=Player.ghosts[i].getY();
 				int BX=this.x;
 				int BY=this.y;
-				int a=(GX-BX)*(GX-BX)+(GY-BY)*(GY-BY);
-				System.err.println(a);
 				if((GX-BX)*(GX-BX)+(GY-BY)*(GY-BY)<1760*1760){// && (GX-BX)*(GX-BX)+(GY-BY)*(GY-BY)>900
 					answer="BUST "+Player.ghosts[i].getID();
+					System.err.println("bust");
 					return true;
 				}
 				
@@ -200,33 +247,50 @@ class Buster {//My busters
 		//System.err.println(Player.ghosts[closestGhostID].getX());
 		if (closestGhostID==-1){return false;}
 		answer="MOVE "+Player.ghosts[closestGhostID].getX()+" "+Player.ghosts[closestGhostID].getY();
+		System.err.println("go");
 		return true;
 	}
 	
 	
 	private void travel() {//peut être fait de façon beaucoups plus efficace.
 		/*
-		 * les x a checker vont de 2000 à 16000 soit 8 étapes 0 à 7
-		 * les y a checker vont de 2000 à 8000  soit 4 étapes 0 à 3
+		 * les x a checker vont de 2000 à 14000 soit 7 étapes 1 à 7
+		 * les y a checker vont de 2000 à 6000  soit 3 étapes 1 à 3
 		 */
+			int boardTravel=Player.boardTravelOdd;
+			if(id%2==0){
+				boardTravel=Player.boardTravelEven;
+			}		
+		
 		int xTravel;//=Player.boardTravel%7;
 		int yTravel;//=Player.boardTravel/7;
-		/*xTravel+=1;
-		yTravel+=1;
-		if(yTravel%2==0){xTravel=9-xTravel;}
-		xTravel*=2000;
-		yTravel*=2000;*/
 		do{
-			Player.boardTravel++;
-			xTravel=Player.boardTravel%7;
-			yTravel=Player.boardTravel/7;
+
+			xTravel=boardTravel%8;
+			yTravel=(boardTravel/7)%4;
 			xTravel+=1;
 			yTravel+=1;
-			if(yTravel%2==0){xTravel=9-xTravel;}
+			if(id%2==0){yTravel=4-yTravel;}
 			xTravel*=2000;
 			yTravel*=2000;
-		}while((xTravel-x)*(xTravel-x)+(xTravel-x)*(xTravel-x)<1000*1000);
+
+			System.err.println((xTravel-x)*(xTravel-x)+(yTravel-y)*(yTravel-y));
+		}while((xTravel-x)*(xTravel-x)+(yTravel-y)*(yTravel-y)<2000*2000);
+			if(id%2==0){
+				Player.boardTravelEven++;
+			}
+			if(id%2!=0){
+				Player.boardTravelOdd++;
+			}
+		System.err.println("travel");
 		answer="MOVE "+xTravel+" "+yTravel;
+	}
+	
+	private boolean canBust(){
+		if (Player.nbturn>stunTurn+20){
+			return true;
+		}
+		return false;
 	}
 
 	
@@ -249,12 +313,15 @@ class Ghost {
 		this.id=id;
 	}
 	
-	public void update(int x, int y,int value) {// TODO change to update
+
+
+	public void update(int x, int y,int value) {
 		this.visible = true;
 		this.x = x;
 		this.y = y;
 		this.value = value;
 	}
+	
 
 //———————————————————
 //——getters setters——
@@ -293,26 +360,42 @@ class OpponentBuster {//My busters
 	private int y;	
 	private int state;
 	private int value;
+	private int stunedTurn;//last turn it has been stuned
 	private boolean visible;
 	
+	public OpponentBuster(int id) {
+		this.id=id;
+		this.stunedTurn=-30;
+	}
 	
-	public void update(int x, int y,int state,int value) {//TODO Change to update
+	public void update(int x, int y,int state,int value) {
 		this.x = x;
 		this.y = y;
 		this.state = state;
 		this.value=value;
 		this.visible=true;
 	}
-
-	public OpponentBuster(int id) {
-		this.id=id;
+	
+	public boolean canBeBusted() {
+		if(state==2){
+			return true;
+		}
+		return false;
 	}
 
 //———————————————————
 //——getters setters——
 //———————————————————
+	public void setStunedTurn(int nbTurn){
+		this.stunedTurn=nbTurn;
+	}
+	
 	public void setVisible(boolean set){
 		this.visible=set;
+	}
+	
+	public boolean getVisible(){
+		return visible;
 	}
 	
 	public int getId() {
